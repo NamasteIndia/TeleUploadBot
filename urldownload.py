1be49372deac6bfb28eed48f5838353087913fa4
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-10 02:29:53 +0530

    Update urldownload.py

--- urldownload.py
+++ urldownload.py
@@ -34,7 +34,7 @@ async def download_coroutine(session, url, file_name, event, start, bot):
 
 **File Size:** {}
 
-**© @Infinity_BOTs**""".format(
+**By @Infinity_BOTs**""".format(
                 url,
                 os.path.basename(file_name).replace("%20", " "),
                 humanbytes(total_length),
